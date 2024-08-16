ac3d6d4653770b5acae3ef6051c3ba5a24227052
Salah ud din <salahuddin5432112345@gmail.com>
2024-08-17 00:24:14 +0500

    first change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
 //import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 
 function App() {
+  const [mode , setMode] = useState('light');
+  const [alert , setAlert] = useState(null);
+
+
+
+  const showAlert = (message , type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "#09121e";
+      showAlert("Dark Mode is Enabled", "success");
+      document.title = 'Textutils - Dark Mode';       //it is used to change the title of the page 
+      // setInterval(() => {
+        // document.title = 'Textutils is best'; 
+      // },2000);  just to set the interval values like here i change the title
+     }
+      
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is Enabled", "success");
+      document.title = 'Textutils - light Mode'; 
+     }
+}
   return (
       <>
-      <Navbar title = "TextUtils"></Navbar>
+       {/* <Router> */}
+      <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}></Navbar>
+      <Alert alert = {alert} />
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze"/>
-      </div>
+     {/* switch can make the user to be on another option without loading the page and it should be rapup into the router tag */}
+      {/* <Routes>   */}
+          {/* <Route exact path="/about" Component={About}> */}
+{/*              */}
+          {/* </Route> */}
+          {/* <Route exact path="/" element={<TextForm heading="Enter text to analyze" mode = {mode} showAlert= {showAlert}/>}> */}
+          
+          {/* </Route> */}
+          <TextForm heading="Enter text to analyze" mode = {mode} showAlert= {showAlert}/>
      
+     
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
       </>
-      
-  )
+  
+  );
 }
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,26 +3,70 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("uppercase is clicked");
         let newText = text.toUpperCase();
         setText(newText);   //right way to change the state
+        props.showAlert("Converted to Uppercase","success");
     }
+    const handleLoClick = ()=>{
+        
+        let newText = text.toLowerCase();
+        setText(newText);   //right way to change the state
+        props.showAlert("Converted to Lowercase","success");
+
+    }
+    function speak (){
+        const newText = text.value;
+        const speech = window.speechSynthesis;
+        const audio = new SpeechSynthesisUtterance(text);
+        speech.speak(audio);
+        props.showAlert("Speaker is on","success");
+
+    }
+   const handleClear = ()=>{
+    let newText = '';
+    setText(newText);
+    props.showAlert("Text is cleared","success");
+
+   }
+    
     const handleOnChange = (event)=>{
         console.log("clicked onchange");
         setText(event.target.value)
     }
 
-    const [text, setText] = useState('Enter text here');
-   
+    const [text, setText] = useState('');
+    const countWords = (str) => {
+      let words;
+      if (text == "") {
+  
+        words = 0;
+      } else {
+        words = str.trim().split(/\s+/).length;
+      }
+      return words;
+    };
   return (
-    <div>    
+    <>   
 
-<div className="mb-3">
+<div className="mb-3" style = {{color : props.mode === 'light'?'black':'white'}}>
     <h1>{props.heading}</h1>
-  <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+  <textarea className="form-control" id="myBox" style = {{color : props.mode === 'light'?'black':'white',
+    backgroundColor : props.mode === 'dark'?'#09121e':'white'
+  }} value={text} onChange={handleOnChange} rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowers</button>
+<button className="btn btn-primary mx-1" onClick={speak}>Listen words</button>
+<button className="btn btn-primary mx-1" onClick={handleClear}>Clear</button>
+<div className="container my-3" style = {{color : props.mode === 'light'?'black':'white'}}>
+    <h1>Your text summary</h1>
+    <p>{countWords(text)} words and {text.length} characters</p>
+    <p>{0.008 * text.split(" ").length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text.length>0?text:"Enter text in text box to preview"}</p>
 </div>
+
+</>
 
   )
 }
